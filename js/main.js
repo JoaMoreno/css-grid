b8d315bae4b8e120c87caccb3af20d719d2e090a
Joaquin Moreno <tec.ignaciomoreno@gmail.com>
2020-01-21 12:47:27 -0300

    grid-column/row

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const boton = document.getElementById("boton-toggle")
+boton.addEventListener("click", toggle)
 function toggle(){
     var grid = document.getElementById("grid-toggle")
     grid.classList.toggle("area-1")
